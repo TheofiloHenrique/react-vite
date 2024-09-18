d2830f6e7cc0e5b6ec06db3ca3a33253a7eb4348
Theofilo Henrique <151052072+TheofiloHenrique@users.noreply.github.com>
2024-09-18 16:56:38 -0300

    Adição de Events.jsx
    Adição do componente de Eventos

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import Cabeça from "./components/Header";
 import Meio from "./components/MainContent";
 import Pe from "./components/Footer";
+import {
+  ClickButton,
+  KeyDownComponent,
+  InputField,
+  FocusBlurComponent,
+  MouseNaDiv,
+} from "./components/Events";
 
 function App() {
   return (
@@ -10,6 +16,11 @@ function App() {
       <Cabeça />
       <Meio />
       <Pe />
+      <ClickButton />
+      <KeyDownComponent />
+      <InputField />
+      <FocusBlurComponent />
+      <MouseNaDiv />
     </>
   );
 }
